a26ae76ffeec152313404b3fc309b383dcb7a1a2
talves <tony@3alves.com>
2022-11-22 14:08:09 -0800

    add: seo component

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://astro-template.netlify.app",
   integrations: [preact({ compat: true }), tailwind(), mdx() ]
 });
