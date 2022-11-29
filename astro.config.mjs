2738770f40dfde58427f0ab46841b15a17873f9e
talves <tony@3alves.com>
2022-11-29 13:51:51 -0800

    add: alpinejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,12 @@ import mdx from "@astrojs/mdx";
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://astro-template.netlify.app",
-  integrations: [preact({ compat: true }), tailwind(), mdx() ]
+  integrations: [preact({
+    compat: true
+  }), tailwind(), mdx(), alpinejs()]
 });
